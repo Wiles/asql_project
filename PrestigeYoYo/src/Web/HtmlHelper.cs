58ffc228cf93550383776bdeba3fb166b6079893
Tom-Bob <kempton.tom@gmail.com>
2012-11-20 20:38:17 -0500

    header shows current user name

--- PrestigeYoYo/src/Web/HtmlHelper.cs
+++ PrestigeYoYo/src/Web/HtmlHelper.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
+﻿///
+///
+///
 
 namespace Prestige
 {
+    using System.Text;
+    using System.Web.Mvc;
+    using System.Web.Mvc.Html;
+    using Prestige.ViewModels;
+
     public static class HtmlHelperExtensions
     {
+        /// <summary>
+        /// Creates the navigation bar.
+        /// </summary>
+        /// <param name="html">The Html Helper.</param>
+        /// <returns>The navigation bar.</returns>
+        public static MvcHtmlString NavigationBar(this HtmlHelper html)
+        {
+            var navigation = new NavButtonModel[]
+            {
+                new NavButtonModel("Reports", "Index", "Report"),
+                new NavButtonModel("Scheduling", "Index", "Schedule"),
+                new NavButtonModel("Management", "Index", "Management"),
+                new NavButtonModel("Administration", "Index", "Admin")
+            };
+
+            var controller = html.ViewContext.Controller.ValueProvider
+                                .GetValue("controller").RawValue.ToString();
+
+            var sb = new StringBuilder("<ul id='menu'>");
+
+            foreach (var nav in navigation)
+            {
+                if (string.Compare(controller, nav.Controller, true) == 0)
+                {
+                    sb.Append("<li class='selected'>");
+                }
+                else
+                {
+                    sb.Append("<li>");
+                }
+
+                sb.Append(
+                        html.ActionLink(nav.Title, nav.Action, nav.Controller)
+                                .ToHtmlString() + "</li>");
+            }
+
+            sb.Append("</ul>");
+            return MvcHtmlString.Create(sb.ToString());
+        }
     }
 }
---
+++ PrestigeYoYo/src/Web/ViewModels/NavButtonModel.cs
@@ -0,0 +1,47 @@
+﻿///
+///
+///
+
+namespace Prestige.ViewModels
+{
+    public class NavButtonModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="NavButtonModel" /> class.
+        /// </summary>
+        /// <param name="title">The title.</param>
+        /// <param name="action">The action.</param>
+        /// <param name="controller">The controller.</param>
+        public NavButtonModel(string title, string action, string controller)
+        {
+            this.Title = title;
+            this.Action = action;
+            this.Controller = controller;
+        }
+
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
+        public string Title { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the controller.
+        /// </summary>
+        /// <value>
+        /// The controller.
+        /// </value>
+        public string Controller { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the action.
+        /// </summary>
+        /// <value>
+        /// The action.
+        /// </value>
+        public string Action { get; private set; }
+    }
+}
