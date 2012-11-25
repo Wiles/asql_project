535055b82804f0a61c2937766e4040cc91e7537b
Hekar <hekark@gmail.com>
2012-11-25 00:45:50 -0500

    still working on pareto chart

--- PrestigeYoYo/src/Web/Controllers/ReportController.cs
+++ PrestigeYoYo/src/Web/Controllers/ReportController.cs
@@ -2,6 +2,15 @@
 using System.Web.Mvc;
 using AutoMapper;
 using Prestige.Services;
+using DotNet.Highcharts.Options;
+using DotNet.Highcharts.Helpers;
+using DotNet.Highcharts.Enums;
+using System.Drawing;
+using DotNet.Highcharts;
+using System.Linq;
+using Prestige.DB.Models;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Prestige.Controllers
 {
@@ -19,11 +28,18 @@ namespace Prestige.Controllers
         public ReportController(
                 IMappingEngine mapper,
                 IProductService productService,
-                IProductionEntryService entryService) : base(mapper)
+                IProductionStageService productionStageService,
+                IProductionEntryService entryService)
+            : base(mapper)
         {
             if (productService == null)
             {
                 throw new ArgumentNullException("productService");
+            }
+
+            if (productionStageService == null)
+            {
+                throw new ArgumentNullException("productionStageService");
             }
 
             if (entryService == null)
@@ -32,16 +48,25 @@ namespace Prestige.Controllers
             }
 
             this.ProductService = productService;
+            this.ProductionStageService = productionStageService;
             this.EntryService = entryService;
         }
 
         /// <summary>
-        /// Gets or sets the person service.
+        /// Gets or sets the product service.
         /// </summary>
         /// <value>
-        /// The person service.
+        /// The product service.
         /// </value>
         private IProductService ProductService { get; set; }
+
+        /// <summary>
+        /// Gets or sets the production stage service.
+        /// </summary>
+        /// <value>
+        /// The production stage service.
+        /// </value>
+        private IProductionStageService ProductionStageService { get; set; }
 
         /// <summary>
         /// Gets or sets the entry service.
@@ -58,7 +83,172 @@ namespace Prestige.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Production Reports";
-            return View();
+
+            DotNet.Highcharts.Highcharts chart = new DotNet.Highcharts.Highcharts("chart")
+            .SetXAxis(new XAxis
+            {
+                Categories = new[] { "Jan", "Feb", "Mar", "Apr", "May", 
+                    "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" }
+            })
+            .SetSeries(new Series
+            {
+                Data = new Data(new object[] { 29.9, 71.5, 106.4, 129.2, 
+                    144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4 })
+            });
+
+            return View(chart);
+        }
+
+        public ActionResult FirstYield()
+        {
+            ViewBag.Title = "First Time Yield Report";
+
+            var results = EntryService.List().ToArray().AsQueryable()
+                    .Where(e => !e.Stages.Any(s => s.ProductFlaw != null))
+                    .SelectMany(e => e.Stages)
+                    .GroupBy(e => e.Station)
+                    .OrderBy(g => g.Count())
+                    .Select(g => new { Name = g.Key.Identifier, Y = g.Count() })
+                    .ToArray();
+
+            return View(PieChart("First Time Yields", "Station First Time Yields", results));
+        }
+
+        public ActionResult FinalYield()
+        {
+            ViewBag.Title = "Final Yield Report";
+
+            var results = EntryService.List().ToArray().AsQueryable()
+                    .Where(e => !e.Stages.Any(s => s.ProductFlaw != null && s.ProductFlaw.Decision != "Rework"))
+                    .SelectMany(e => e.Stages)
+                    .GroupBy(e => e.Station)
+                    .OrderBy(g => g.Count())
+                    .Select(g => new { Name = g.Key.Identifier, Y = g.Count() })
+                    .ToArray();
+
+            return View(PieChart("First Time Yields", "Station First Time Yields", results));
+        }
+
+        private Highcharts PieChart(string chartName, string axisName, object[] results)
+        {
+            Highcharts chart = new Highcharts("chart")
+                .InitChart(new Chart { PlotShadow = false })
+                .SetTitle(new Title { Text = chartName })
+                .SetTooltip(new Tooltip { Formatter = "function() { return '<b>'+ this.point.name +'</b>: '+ this.percentage.toFixed(2) +' %'; }" })
+                .SetPlotOptions(new PlotOptions
+                {
+                    Pie = new PlotOptionsPie
+                    {
+                        AllowPointSelect = true,
+                        Cursor = Cursors.Pointer,
+                        DataLabels = new PlotOptionsPieDataLabels
+                        {
+                            Color = ColorTranslator.FromHtml("#000000"),
+                            ConnectorColor = ColorTranslator.FromHtml("#000000"),
+                            Formatter = "function() { return '<b>'+ this.point.name +'</b>: '+ this.percentage.toFixed(2) +' %'; }"
+                        }
+                    }
+                })
+                .SetSeries(new Series
+                {
+                    Type = ChartTypes.Pie,
+                    Name = axisName,
+                    Data = new Data(results)
+                });
+
+            return chart;
+        }
+
+        public ActionResult DefectCategories()
+        {
+            ViewBag.Title = "Defect Categories Report";
+            
+            var defectCategories = EntryService.List().ToArray().AsQueryable()
+                .SelectMany(s => s.Stages)
+                .Where(s => s.ProductFlaw != null)
+                .GroupBy(s => s.ProductFlaw)
+                .OrderByDescending(g => g.Count())
+                .Select(g => new { Name = g.Key.Identifier, NumberOfDefects = g.Count() })
+                .ToArray();
+
+            var barValues = defectCategories.Select(d => d.NumberOfDefects);
+
+            var linesValues = new List<int>();
+            foreach (var num in barValues)
+            {
+                var next = linesValues.LastOrDefault() + num;
+                linesValues.Add(next);
+            }
+
+            Highcharts chart = new Highcharts("chart")
+           .InitChart(new Chart { ZoomType = ZoomTypes.Xy })
+           .SetTitle(new Title { Text = "Defect Categories" })
+           .SetSubtitle(new Subtitle { Text = "" })
+           .SetXAxis(new XAxis { Categories = defectCategories.Select(d => d.Name).ToArray() })
+           .SetYAxis(new[]
+            {
+                new YAxis
+                {
+                    Labels = new YAxisLabels
+                    {
+                        Formatter = "function() { return this.value; }",
+                        Style = "color: '#89A54E'"
+                    },
+                    Title = new XAxisTitle
+                    {
+                        Text = "Count",
+                        Style = "color: '#89A54E'"
+                    }
+                },
+                new YAxis
+                {
+                    Labels = new YAxisLabels
+                    {
+                        Formatter = "function() { return this.value; }",
+                        Style = "color: '#4572A7'"
+                    },
+                    Title = new XAxisTitle
+                    {
+                        Text = "Defect Categories",
+                        Style = "color: '#4572A7'"
+                    },
+                    Opposite = true
+                }
+            })
+           .SetTooltip(new Tooltip
+           {
+               Formatter = "function() { return ''+ this.x +': '+ this.y; }"
+           })
+           .SetLegend(new Legend
+           {
+               Layout = Layouts.Vertical,
+               Align = HorizontalAligns.Left,
+               X = 120,
+               VerticalAlign = VerticalAligns.Top,
+               Y = 100,
+               Floating = true,
+               BackgroundColor = ColorTranslator.FromHtml("#FFFFFF")
+           })
+           .SetSeries(new[]
+            {
+                new Series
+                {
+                    Name = "Count",
+                    Color = ColorTranslator.FromHtml("#4572A7"),
+                    Type = ChartTypes.Column,
+                    YAxis = 1,
+                    Data = new Data(barValues.Cast<object>().ToArray())
+                },
+                new Series
+                {
+                    Name = "Defect Categories",
+                    Color = ColorTranslator.FromHtml("#89A54E"),
+                    Type = ChartTypes.Spline,
+                    Data = new Data(linesValues.Cast<object>().ToArray())
+                }
+            });
+
+            return View(chart);
         }
     }
 }
